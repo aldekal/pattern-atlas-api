5dc4bd6c94237cf368779a5fd49e4c44c8f65ac8
Michael Falkenthal <michael.falkenthal@googlemail.com>
2019-11-18 11:22:18 +0100

    Add exception handling for *NotFoundExceptions
    Signed-off-by: Michael Falkenthal <michael.falkenthal@googlemail.com>

---
+++ src/main/java/com/patternpedia/api/rest/exception/RestResponseExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.patternpedia.api.rest.exception;
+
+import com.patternpedia.api.exception.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseExceptionHandler
+        extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {
+            PatternLanguageNotFoundException.class,
+            PatternNotFoundException.class,
+            PatternSchemaNotFoundException.class,
+            PatternViewNotFoundException.class,
+            DirectedEdgeNotFoundException.class,
+            UndirectedEdgeNotFoundException.class
+    })
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+}
