696959999e9bffd41d2b5c0a5cabaf2c86a1d005
Philipp Wundrack <philipp.wundrack@live.de>
2022-05-10 16:30:39 +0200

    fix error when deleting pattern from pattern language

--- src/main/java/io/github/patternatlas/api/service/PatternLanguageServiceImpl.java
+++ src/main/java/io/github/patternatlas/api/service/PatternLanguageServiceImpl.java
@@ -232,7 +232,7 @@ public class PatternLanguageServiceImpl implements PatternLanguageService {
         }
 
         // 2. Remove Pattern from Views it is included
-        pattern.setPatternViews(null);
+        pattern.getPatternViews().clear();
         pattern = this.patternService.updatePattern(pattern);
 
         // 3. Remove pattern
